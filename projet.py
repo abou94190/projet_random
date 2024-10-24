cd5e20d72b9ba012aa4aa71dce422808c8283bcb
Abou Ly <166803585+abou94190@users.noreply.github.com>
2024-10-24 11:06:30 +0200

    ajout explication

--- projet.py
+++ projet.py
@@ -7,6 +7,8 @@ import random
 
 def deviner_nombre():
     # Générer un nombre aléatoire entre 1 et 100 ,avec randint module de la library randam
+    # init variable tentative max
+    # init variable tentatives
     nombre_a_deviner = random.randint(1, 100)
     tentatives_max = 10
     tentatives = 0
@@ -16,6 +18,7 @@ def deviner_nombre():
 
     while tentatives < tentatives_max:
         # Demander à l'utilisateur de deviner
+        # rajoute +1 à la variable tentatives pour compter le nbr de tentatives
         try:
             devine = int(input(f"Tentative {tentatives + 1}: Entrez votre nombre: "))
         except ValueError:
@@ -25,15 +28,19 @@ def deviner_nombre():
         tentatives += 1
 
         # Comparer le nombre deviné avec le nombre à deviner
+        # si devine est inférieur au nombre a deviner
         if devine < nombre_a_deviner:
             print("Trop petit.")
+        # si devine supérieur au nombre a deviner
         elif devine > nombre_a_deviner:
             print("Trop grand.")
         else:
             print(f"Bravo, vous avez deviné le nombre en {tentatives} tentatives !")
             break
 
-    # Si l'utilisateur n'a pas trouvé le nombre
+    # Si l'utilisateur n'a pas trouvé le nombre , si toutes les tentative sont utilisées
+    # et devine est different du nombre_a_deviner
+    # print la réponse
     if tentatives == tentatives_max and devine != nombre_a_deviner:
         print(f"Game Over. Le nombre était {nombre_a_deviner}.")
 
@@ -42,7 +49,7 @@ def deviner_nombre():
 
 def rejouer():
     while True:
-        choix = input("Voulez-vous rejouer ? (o/n): ").lower()
+        choix = input("rejouer ? (o/n): ").lower()
         if choix == 'o':
             return True
         elif choix == 'n':
@@ -60,7 +67,7 @@ def jeu():
         if not rejouer():
             break
 
-# Lancer le jeu
+# Lancer le jeu , si le jeu est exectuer dans main ou en tant que module
 
 
 if __name__ == "__main__":
